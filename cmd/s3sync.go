a1d4b3f8e0f72bc3a5aa5980e4e1430ea20e32e2
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-02-12 11:00:56 +0900

    implement simple upload, download.

--- cmd/s3sync.go
+++ cmd/s3sync.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+    "bytes"
 	"context"
 	"flag"
 	"fmt"
@@ -9,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/aws/aws-sdk-go-v2/feature/s3/manager"
 )
 
 func main() {
@@ -35,11 +37,13 @@ func _main() {
 	fmt.Println("upload subcommand.")
 	fmt.Println("   force:", *forceUpload)
 	fmt.Println("   mode:", *modeUpload)
+	s3sync_upload()
     case "download":
 	download.Parse(os.Args[2:])
 	fmt.Println("download subcommand")
 	fmt.Println("   force:", *forceDownload)
 	fmt.Println("   mode:", *modeDownload)
+	s3sync_download()
     default:
 	fmt.Println("expected 'upload' or 'download' subcommands.")
 	os.Exit(1)
@@ -48,13 +52,46 @@ func _main() {
     ls()
 }
 
-func ls() {
-    cfg, err := config.LoadDefaultConfig(context.TODO())
+func s3sync_upload() {
+    client := client()
+    uploader := manager.NewUploader(client, func(u *manager.Uploader) {
+	u.BufferProvider = manager.NewBufferedReadSeekerWriteToPool(25 * 1024 * 1024)
+    })
+
+    _, err := uploader.Upload(context.TODO(), &s3.PutObjectInput {
+	Bucket: aws.String("s3sync-cli"),
+	Key: aws.String("key1"),
+	Body: bytes.NewReader([]byte("hoge uga ahe")),
+    })
     if err != nil {
 	panic(err)
     }
+}
 
-    client := s3.NewFromConfig(cfg)
+func s3sync_download() {
+    f, err := os.Create("download.txt")
+    if err != nil {
+	panic(err)
+    }
+    defer f.Close()
+
+    client := client()
+
+    downloader := manager.NewDownloader(client, func(d *manager.Downloader) {
+	d.PartSize = 64 * 1024 * 1024
+    })
+    n ,err := downloader.Download(context.TODO(), f, &s3.GetObjectInput {
+	Bucket: aws.String("s3sync-cli"),
+	Key: aws.String("key1"),
+    })
+    if err != nil {
+	panic(err)
+    }
+    fmt.Println("download %d bytes\n", n)
+}
+
+func ls() {
+    client := client()
 
     output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input {
 	Bucket: aws.String("s3sync-cli"),
@@ -66,4 +103,21 @@ func ls() {
     for _, object := range output.Contents {
 	fmt.Printf("key=%s, size=%d \n", aws.ToString(object.Key), object.Size)
     }
+}
+
+func client() *s3.Client {
+    cfg, err := config.LoadDefaultConfig(context.TODO())
+    if err != nil {
+	panic(err)
+    }
+
+    client := s3.NewFromConfig(cfg)
+    return client
+}
+
+func loadConf() {
+    // load
+    // bucket name
+    // local directory
+    // s3 bucket name
 }
--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go-v2 v1.2.0
 	github.com/aws/aws-sdk-go-v2/config v1.1.1
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.0.2 // indirect
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.2.0
 )
