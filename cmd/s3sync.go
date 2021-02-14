b284d16d5f70a63b91d66c48e4e9b2885d0ecaa6
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-02-15 01:17:13 +0900

    a little improved upload, download feature.

--- cmd/s3sync.go
+++ cmd/s3sync.go
@@ -1,12 +1,15 @@
 package main
 
 import (
-	"bytes"
+	//"bytes"
 	"context"
 	"flag"
 	"fmt"
 	"os"
+	"path/filepath"
+	"s3sync/cmd/util"
 	"s3sync/configs"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/config"
@@ -26,6 +29,8 @@ func _main() {
     download := flag.NewFlagSet("download", flag.ExitOnError)
     forceDownload := download.Bool("f", false, "force download")
     modeDownload := download.String("m", "", "mode")
+
+    ls := flag.NewFlagSet("ls", flag.ExitOnError)
 
     if len(os.Args) < 2 {
 	fmt.Println("expected 'upload' or 'download' subcommands.")
@@ -51,51 +56,83 @@ func _main() {
 	fmt.Println("   force:", *forceDownload)
 	fmt.Println("   mode:", *modeDownload)
 	s3sync_download(s)
+    case "ls":
+	ls.Parse(os.Args[2:])
+	fmt.Println("ls subcommand")
+	s3sync_ls(s)
     default:
-	fmt.Println("expected 'upload' or 'download' subcommands.")
+	fmt.Println("expected 'upload' or 'download', 'ls' subcommands.")
 	os.Exit(1)
     }
 }
 
+// maybe use BatchUploadObject
 func s3sync_upload(s *configs.Setting) {
     client := client()
     uploader := manager.NewUploader(client, func(u *manager.Uploader) {
 	u.BufferProvider = manager.NewBufferedReadSeekerWriteToPool(25 * 1024 * 1024)
     })
 
-    _, err := uploader.Upload(context.TODO(), &s3.PutObjectInput {
-	Bucket: aws.String(s.BucketName),
-	Key: aws.String("test/bbb.txt"),
-	Body: bytes.NewReader([]byte("hoge uga ahe")),
-    })
-    if err != nil {
-	panic(err)
+    list := util.ListDir(s.LocalDir)
+    for _, file := range list {
+	body, err := os.Open(s.LocalDir + file)
+	if err != nil {
+	    panic(err)
+	}
+	//fmt.Println(file)
+
+	_, err = uploader.Upload(context.TODO(), &s3.PutObjectInput {
+	    Bucket: aws.String(s.BucketName),
+	    Key: aws.String(s.S3Dir + file),
+	    Body: body,
+	})
+	if err != nil {
+	    panic(err)
+	}
     }
 }
 
+// maybe use BatchDownloadObject
 func s3sync_download(s *configs.Setting) {
-    f, err := os.Create("download.txt")
-    if err != nil {
-	panic(err)
-    }
-    defer f.Close()
-
     client := client()
 
     downloader := manager.NewDownloader(client, func(d *manager.Downloader) {
 	d.PartSize = 64 * 1024 * 1024
     })
-    n ,err := downloader.Download(context.TODO(), f, &s3.GetObjectInput {
-	Bucket: aws.String(s.BucketName),
-	Key: aws.String("test/aaa.txt"),
-    })
-    if err != nil {
-	panic(err)
+
+    // get download list from ls
+    downloadList := s3sync_ls(s)
+
+    // remove directory
+    downloadListRemoveDir := make([]string, 0)
+    for _, d := range downloadList {
+	if !strings.HasSuffix(d, "/") {
+	    downloadListRemoveDir = append(downloadListRemoveDir, d)
+	}
     }
-    fmt.Printf("download %d bytes\n", n)
+
+    // create file
+    for _, s3File := range downloadListRemoveDir {
+	localPath := s.LocalDir + strings.TrimPrefix(s3File, s.S3Dir)
+	os.MkdirAll(filepath.Dir(localPath), 0777)
+	localFile, err := os.Create(localPath)
+	if err != nil {
+	    panic(err)
+	}
+	defer localFile.Close()
+
+	n ,err := downloader.Download(context.TODO(), localFile, &s3.GetObjectInput {
+	    Bucket: aws.String(s.BucketName),
+	    Key: aws.String(s3File),
+	})
+	if err != nil {
+	    panic(err)
+	}
+	fmt.Printf("download %d bytes\n", n)
+    }
 }
 
-func ls(s *configs.Setting) {
+func s3sync_ls(s *configs.Setting) []string {
     client := client()
 
     output, err := client.ListObjectsV2(context.TODO(), &s3.ListObjectsV2Input {
@@ -106,9 +143,13 @@ func ls(s *configs.Setting) {
 	panic(err)
     }
 
+    ret := make([]string, 0)
     for _, object := range output.Contents {
 	fmt.Printf("key=%s, size=%d \n", aws.ToString(object.Key), object.Size)
+	ret = append(ret, aws.ToString(object.Key))
     }
+
+    return ret
 }
 
 func client() *s3.Client {
@@ -119,11 +160,4 @@ func client() *s3.Client {
 
     client := s3.NewFromConfig(cfg)
     return client
-}
-
-func loadConf() {
-    // load
-    // bucket name
-    // local directory
-    // s3 bucket name
 }
--- go.mod
+++ go.mod
@@ -5,7 +5,7 @@ go 1.15
 require (
 	github.com/aws/aws-sdk-go-v2 v1.2.0
 	github.com/aws/aws-sdk-go-v2/config v1.1.1
-	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.0.2 // indirect
+	github.com/aws/aws-sdk-go-v2/feature/s3/manager v1.0.2
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.2.0
 	gopkg.in/yaml.v2 v2.2.8
 )
